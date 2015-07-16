81c78b3f245b884628692049996012197e1a96d7
aleks-programmer <Aleksaaa555@gmail.com>
2015-07-16 09:31:47 +0400

    Work Tracker App

---
+++ EJBM/src/simpleperson/UserDetails.java
@@ -0,0 +1,33 @@
+package simpleperson;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+/**
+ * User Detals
+ */
+@XmlRootElement(name = "userDetails")
+public class UserDetails implements Serializable {
+
+    @XmlElement
+    private Integer userID;
+    @XmlElement
+    private String userName;
+
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
